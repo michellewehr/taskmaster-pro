15e42572d3cb43cd698251902af18e6160170c3d
Michelle Wehr <michelle_wehr@icloud.com>
2021-08-14 11:20:14 -0400

    set setInterval function to run on each task every 30 min

--- assets/js/script.js
+++ assets/js/script.js
@@ -298,4 +298,10 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+//set interval function 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
